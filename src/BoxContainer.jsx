545672e33f4c38422699c42cc970887e1a4531f4
Dariusz Okonski <darek200180@gmail.com>
2019-11-24 17:00:10 +0000

    add BoxContainer component

---
+++ src/BoxContainer.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Box from './Box';
+import './BoxContainer.css';
+
+class BoxContainer extends Component {
+  static defaultProps = {
+    numBoxes: 16,
+    allColors: ["purple", "magenta", "violet", "pink", "green", "yellow", "red", "blue", "brown", "crimson", "royalblue"],
+  }
+
+  render() {
+    const boxes = Array.from({ length: this.props.numBoxes }).map(() =>
+      <Box colors={this.props.allColors} />)
+
+    return (
+      <div className="BoxContainer">
+        {
+          boxes
+        }
+      </div>
+    );
+  }
+}
+
+export default BoxContainer;
