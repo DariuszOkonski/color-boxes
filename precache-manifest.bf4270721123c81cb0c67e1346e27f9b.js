e466b9dc01abb627310dfbde198643c6ff012704
Dariusz Okonski <darek200180@gmail.com>
2019-11-24 16:28:45 +0000

    Updates

---
+++ precache-manifest.bf4270721123c81cb0c67e1346e27f9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5131103db238d64b96a77a4ee32c29",
+    "url": "/color-boxes/index.html"
+  },
+  {
+    "revision": "77ac71715d888c8928d6",
+    "url": "/color-boxes/static/css/main.3452505e.chunk.css"
+  },
+  {
+    "revision": "9f9ad6ff23d0f77d6bfa",
+    "url": "/color-boxes/static/js/2.24a5dcf0.chunk.js"
+  },
+  {
+    "revision": "77ac71715d888c8928d6",
+    "url": "/color-boxes/static/js/main.521a92f4.chunk.js"
+  },
+  {
+    "revision": "ef88545ead34efdd9e1e",
+    "url": "/color-boxes/static/js/runtime-main.0c7c5647.js"
+  }
+]);
